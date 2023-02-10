e03b244589897f3092123b7d2ac093fd61130d0e
BHANUPRASAD DERANGULA <53010354+bhanu141549@users.noreply.github.com>
2023-02-10 20:40:50 +0530

    Add files via upload

---
+++ employee-management-system/src/main/java/com/employee-system/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employee_system.employee.repository;
+
+import com.employee_system.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
